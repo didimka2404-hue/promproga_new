34f492f6246ff7d402e48cb9682ef93d83014279
Dima Krugliatsov <didimka2404@gmail.com>
2026-05-12 21:22:40 +0300

    Initial commit: factorial calculator in C

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <locale.h>
+
+int factorial(int n) {
+    if (n < 0) {
+        return 0;
+    }
+    unsigned long long result = 1;
+    for (int i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    printf("Добрый день, молодежь,\n");
+    printf("последний commit ура\n");
+    int num;
+    setlocale(LC_ALL, "RUSSIAN");
+
+    printf("Введите целое неотрицательное число(pls): ");
+    if (scanf("%d", &num) != 1) {
+        printf("Ошибка ввода. вы ввели недопустимое начение\n");
+        return 1;
+    }
+
+    int fact = factorial(num);
+    printf("Факториал числа %d равен %d\n", num, fact);
+
+    return 0;
+}
