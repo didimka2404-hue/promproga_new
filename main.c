493fbadaa7e4bb033b235d19a10d1b2239eab0f5
Dima Krugliatsov <didimka2404@gmail.com>
2026-05-12 21:26:48 +0300

    Beta: change greeting differently

--- main.c
+++ main.c
@@ -23,7 +23,7 @@ int power(int base, int exp) {
 }
 
 int main() {
-    printf("Добрый день, молодежь,\n");
+    printf("BETA: Добрый день, молодежь!!\n");
     printf("последний commit ура\n");
     int num;
     setlocale(LC_ALL, "RUSSIAN");
